64c5a756b89d8d595378949b937ab0272c970f0a
Ibrahim Khalilov <Ibo@Guljannats-MacBook-Air.local>
2024-01-22 15:30:54 -0500

    first commit

---
+++ src/SkipListProjectTest.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+
+import student.TestCase;
+
+public class SkipListProjectTest extends TestCase {
+
+	/**
+	 * Setup tests
+	 */
+	public void setUp() {
+		// Nothing Here
+	}
+
+	public void testMain() {
+		String[] args = { "duplicate_test.txt" };
+		systemOut().clearHistory();
+		SkipListProject.main(args);
+		String output = systemOut().getHistory();
+		String referenceOutput = null;
+		try {
+			referenceOutput = SkipListProject.readFile("duplicate_testout.txt");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		assertFuzzyEquals(referenceOutput, output);
+	}
+}
