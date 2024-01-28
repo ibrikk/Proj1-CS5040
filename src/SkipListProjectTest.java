a6523eb3077b0151c950a35b7f3c14efc2761008
Ibrahim Khalilov <Ibo@guljannats-air.mynetworksettings.com>
2024-01-27 20:37:20 -0500

    write insert tests

--- src/SkipListProjectTest.java
+++ src/SkipListProjectTest.java
@@ -14,16 +14,17 @@ public class SkipListProjectTest extends TestCase {
 
     public void testMain() {
 // String[] args = { "duplicate_test.txt" };
-        String[] args = { "P1test1.txt" };
+        String[] args = { "insert_basic.txt" };
+//        String[] args = { "P1test1.txt" };
         systemOut().clearHistory();
         SkipListProject.main(args);
         String output = systemOut().getHistory();
         String referenceOutput = null;
         try {
-            referenceOutput = SkipListProject.readFile("duplicate_testout.txt");
+//            referenceOutput = SkipListProject.readFile("duplicate_testout.txt");
+            referenceOutput = SkipListProject.readFile("insert_basicout.txt");
         }
         catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
--- src/SkipListTest.java
+++ src/SkipListTest.java
@@ -5,7 +5,20 @@ public class SkipListTest extends TestCase {
         // Nothing Here
     }
 
+
     public void testInsert() {
+        SkipList skl = new SkipList();
+        Rectangle rec1 = new Rectangle(0, 0, 100, 100);
+        Rectangle rec2 = new Rectangle(99, 99, 924, 924);
+        KVPair<String, Rectangle> it1 = new KVPair<>("A", rec1);
+        KVPair<String, Rectangle> it2 = new KVPair<>("B", rec2);
+        skl.insert(it1);
+        assertEquals(skl.size(), 1);
         
+        skl.insert(it2);
+        assertEquals(skl.size(), 2);
+        
+        skl.insert(it1);
+        assertEquals(skl.size(), 3);
     }
 }
