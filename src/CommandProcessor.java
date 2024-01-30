6b0a02300f1db83152a35cb4a91dee336cbbfccc
Ibrahim Khalilov <Ibo@guljannats-air.mynetworksettings.com>
2024-01-29 23:14:28 -0500

    improve style points

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -75,9 +75,8 @@ public class CommandProcessor {
 //
 // }
         else if (command.equals("regionsearch")) {
-            data.regionsearch(Integer.parseInt(arr[1]),
-                Integer.parseInt(arr[2]), Integer.parseInt(arr[3]), Integer
-                .parseInt(arr[4]));
+            data.regionsearch(Integer.parseInt(arr[1]), Integer.parseInt(
+                arr[2]), Integer.parseInt(arr[3]), Integer.parseInt(arr[4]));
         }
         else if (command.equals("intersections")) {
             // calls the intersections method, no parameters to be passed
